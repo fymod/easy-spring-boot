196a0b62ed6b6bb2973cd973abc1bb21356407d0
fymod <793064859@qq.com>
2018-06-26 14:51:33 +0800

    no commit message

--- springboot-qiniu/springboot-qiniu/src/main/java/com/fymod/qiniu/config/APPConstants.java
+++
@@ -1,9 +0,0 @@
-package com.fymod.qiniu.config;
-
-public class APPConstants {
-
-	public static final String AK = "v-UwIPFYtZJqj_LOrAMhmTaVWv7MZeEUyfpW_bt2";
-	public static final String SK = "GrT-qeElVDay8J85UeliJoSTn2K73ATGf1Mtboqb";
-	
-	public static String defaultUrl = "http://pawvgpj03.bkt.clouddn.com";
-}
