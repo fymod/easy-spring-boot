4b70b4541ac2b655cda2faa6f961b275a7a59c01
fymod <793064859@qq.com>
2018-10-09 22:36:51 +0800

    springboot-swagger

---
+++ springboot-swagger/src/main/java/com/fymod/swagger/SpringbootSwaggerApplication.java
@@ -0,0 +1,12 @@
+package com.fymod.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSwaggerApplication.class, args);
+	}
+}
